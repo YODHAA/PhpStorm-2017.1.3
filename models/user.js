b8ecf99320b5f7fe0a9cf2b7a763e81e5e527994
Saurabh Singh <saurabhsingh4589@gmail.com>
2019-03-23 17:49:42 +0530

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,49 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
+
+// no need to initialse the mongoose object here its there in app.js
+// User Schema
+var UserSchema = mongoose.Schema({
+	username: {
+		type: String,
+		index:true
+	},
+	password: {
+		type: String
+	},
+	email: {
+		type: String
+	},
+	name: {
+		type: String
+	}
+});
+
+var User = module.exports = mongoose.model('User', UserSchema);
+
+// store hash in your password database
+
+module.exports.createUser = function(newUser, callback){
+	bcrypt.genSalt(10, function(err, salt) {
+	    bcrypt.hash(newUser.password, salt, function(err, hash) {
+	        newUser.password = hash;
+	        newUser.save(callback);
+	    });
+	});
+}
+
+module.exports.getUserByUsername = function(username, callback){
+	var query = {username: username};
+	User.findOne(query, callback);
+}
+
+module.exports.getUserById = function(id, callback){
+	User.findById(id, callback);
+}
+
+module.exports.comparePassword = function(candidatePassword, hash, callback){
+	bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
+    	if(err) throw err;
+    	callback(null, isMatch);
+	});
+}
